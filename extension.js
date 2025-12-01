df9c9024275fbefc8afe25166cc9ba6709a65cc0
Anas-hessein <anas.hessen15@gmail.com>
2025-12-01 09:26:54 +0200

    new

---
+++ extension.js
@@ -0,0 +1,164 @@
+let vscode = require("vscode")
+const JSZip = require("jszip")
+const fs = require("fs")
+const path = require("path")
+
+function activate(context) {
+
+    let cmd = vscode.commands.registerCommand("playground.open", () => {
+
+        let panel = vscode.window.createWebviewPanel(
+            "playground",
+            "Frountend Playground",
+            vscode.ViewColumn.One,
+            {
+                enableScripts: true
+            }
+        )
+
+        panel.webview.html = getHtml(panel.webview, context)
+
+        panel.webview.onDidReceiveMessage((msg) => {
+
+            if(msg.type === "run") {
+                panel.webview.postMessage({
+                    type: "preview",
+                    html: msg.html,
+                    css: msg.css,
+                    js: msg.js
+                })
+            }
+            if (msg.type === "export") {
+                exportProjectAsZip(context, msg)
+            }
+            if (msg.type === "import") {
+                importProject(panel)
+            }
+        })
+    })
+
+    context.subscriptions.push(cmd)
+}
+
+exports.activate = activate
+
+async function exportProjectAsZip(context, data) {
+
+    let zip = new JSZip()
+
+    zip.file("index.html", data.html)
+    zip.file("style.css", data.css)
+    zip.file("script.js", data.js)
+
+    let zipBlob = await zip.generateAsync({ type: "nodebuffer"})
+
+    let uri = await vscode.window.showSaveDialog({  
+        saveLabel: "Save Project As ZIP"
+    })
+
+    if (!uri) return
+
+    fs.writeFileSync(uri.fsPath, zipBlob) 
+
+    vscode.window.showInformationMessage("Project exported successfully!")
+}
+
+async function importProject(panel) {
+
+    let uri = await vscode.window.showOpenDialog({
+        canSelectMany: false,
+        filters: {
+            'ZIP files': ['zip']
+        },
+        openLabel: "Select ZIP file"
+    })
+
+    if (!uri || uri.length === 0) return
+
+    let data = fs.readFileSync(uri[0].fsPath)
+
+    let zip = await JSZip.loadAsync(data)
+
+    let html = ""
+    let css = ""
+    let js = ""
+
+    if (zip.files["index.html"]) {
+        html = await zip.files["index.html"].async("string")
+    }
+
+    if (zip.files["style.css"]) {
+        css = await zip.files["style.css"].async("string") 
+    }
+
+    if (zip.files["script.js"]) {
+        js = await zip.files["script.js"].async("string")
+    }
+
+    panel.webview.postMessage({
+        type: "imported",
+        html: html,
+        css: css,
+        js: js
+    })
+
+    vscode.window.showInformationMessage("Project Imported Successfully!")
+}
+
+function getHtml(webview,context) {
+    let scriptPath = webview.asWebviewUri(
+        vscode.Uri.file(path.join(context.extensionPath, "webview", "main.js"))
+    );
+    let cssPath = webview.asWebviewUri(
+        vscode.Uri.file(path.join(context.extensionPath, "webview", "style.css"))
+    );
+
+    return `
+    <!DOCTYPE html>
+    <htlm>
+    <head>
+        <link rel="stylesheet" href="${cssPath}">
+    </head>
+    
+    <body>
+        <div class="container">
+            <div class="left-panel">
+                <div class="editor-container">
+                    <div class="tabs">
+                        <button class="tab active" data-tab="html">HTML</button>
+                        <button class="tab" data-tab="css">CSS</button>
+                        <button class="tab" data-tab="js">JS</button>
+                        <button class="tab" data-tab="jsx">JSX</button>
+                        <button id="exportBtn">Export ZIP</button>
+                        <button id="importBtn">Import ZIP</button>
+                    </div>
+
+                    <div class="libs">
+                        <button class= "lib-btn" data-lib="react">React</button>
+                        <button class= "lib-btn" data-lib="vue">Vue</button>
+                        <button class= "lib-btn" data-lib="tailwind">Tailwind css</button>
+                    </div>
+
+                    <textarea id="editor"></textarea>
+                
+                </div>
+
+                <div class="console-container">
+                    <h3> Console </h3>
+                    <div id="console" style="background:#1e1e1e;color:white;height:100px;overflow:auto;padding:5px;"></div>
+                </div>
+                
+                <div class="preview-container">
+                    <iframe id="preview"></iframe>
+            </div>
+            </div>
+        </div>
+        
+        <script src="${scriptPath}"></script>
+        
+    </body>
+    </html>
+    `;
+}
+
+
---
+++ webview/main.js
@@ -0,0 +1,170 @@
+const vscode = acquireVsCodeApi()
+
+
+let htmlCode = "<h1> Hello Hackclubber </h1>"
+let cssCode = "h1 { font-family: sans-serif; }"
+let jsCode = "console.log('Hello Hackclubber')"
+let jsxCode = "const root = ReactDOM.createRoot(document.getElementById('root')); \nroot. render(<h1> Hello JSX clubber</h1>);"
+let currentTab = "html"
+let updateTimeout  
+
+let editor = document.getElementById("editor")
+editor.value = htmlCode
+
+document.querySelectorAll(".tab").forEach(btn => {
+  btn.addEventListener("click", () => {
+    document.querySelector(".tab.active").classList.remove("active")
+    btn.classList.add("active")
+
+    currentTab = btn.dataset.tab 
+
+    if (currentTab === "html") editor.value = htmlCode
+    if (currentTab === "css") editor.value = cssCode
+    if (currentTab === "js") editor.value = jsCode
+    if (currentTab === "jsx") editor.value = jsxCode
+  })
+})
+
+editor.addEventListener("input", () => {
+  if (currentTab === "html") htmlCode = editor.value
+  if (currentTab === "css") cssCode = editor.value
+  if (currentTab === "js") jsCode = editor.value 
+  if (currentTab === "jsx") jsxCode = editor.value
+
+  updatePreviewAuto()
+})
+
+function updatePreviewAuto() {
+  clearTimeout(updateTimeout)
+
+  updateTimeout = setTimeout(() => {
+    vscode.postMessage({
+      type: "run",
+      html: htmlCode,
+      css: cssCode,
+      js: jsCode,
+      jsx: jsxCode,
+      libs: enabledLibs
+    })
+  }, 300)
+}
+
+let libraries = {
+    react: `<script src="https://unpkg.com/react@18/umd/react.development.js"></script>
+            <script src="https://unpkg.com/react-dom@18/umd/react-dom.development.js"></script>`,
+    vue: `<script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>`,
+    tailwind: `<script src="https://cdn.tailwindcss.com"></script>`
+}
+
+let enabledLibs = {
+  react: false,
+  vue: false,
+  tailwind: false
+}
+
+document.querySelectorAll(".lib-btn").forEach(btn => {
+  btn.addEventListener("click", () => {
+    let lib = btn.dataset.lib
+
+    if (!enabledLibs[lib]) {
+      enabledLibs[lib] = true
+
+      btn.disabled = true
+      btn.style.opacity = "0.5"
+
+      vscode.postMessage({
+        type: "run",
+        html: htmlCode,
+        css: cssCode,
+        js: jsCode,
+        libs: enabledLibs
+      })
+    }
+  })
+})
+
+document.getElementById("exportBtn").addEventListener("click", () => {
+  vscode.postMessage({
+    type: "export",
+    html: htmlCode,
+    css: cssCode,
+    js: jsCode,
+  })
+})
+
+document.getElementById("importBtn").addEventListener("click", () => {
+  vscode.postMessage({ type: "import" })
+})
+
+window.addEventListener("message", (event) => {
+  let msg = event.data
+
+  if(msg.type === "imported") {
+
+    htmlCode = msg.html
+    cssCode = msg.css
+    jsCode = msg.js
+
+    if (currentTab === "html") editor.value = htmlCode
+    if (currentTab === "css") editor.value = cssCode
+    if (currentTab === "js") editor.value = jsCode
+
+    updatePreviewAuto()
+  }
+})
+
+window.addEventListener("message", (event) => {
+  let msg = event.data
+
+  if (msg.type === "preview") {
+    let iframe = document.getElementById("preview")
+    let consoleDiv = document.getElementById("console")
+    consoleDiv.innerHTML = ''
+     
+    let reactCDN = `<script src="https://unpkg.com/react@18/umd/react.development.js"></script>
+                    <script src="https://unpkg.com/react-dom@18/umd/react-dom.development.js"></script>`
+    
+    let babelCDN = `<script src="https://unpkg.com/@babel/standalone/babel.min.js"></script>`
+    
+    iframe.srcdoc = `
+                    <!DOCTYPE html>
+                    <html>
+                    <head>
+                          ${reactCDN}
+                          ${babelCDN}
+                          <style>
+                          ${msg.css}
+                          </style>
+                    </head>
+                    <body>
+                          ${msg.html}
+
+                          <div id="iframeConsole" style="display:none;"></div>
+
+                          <script>
+
+                          const consoleDiv = parent.document.getElementById('console')
+                          consoleDiv.innerHTML = ''
+
+                          const oldLog = console.log
+                          console.log = function(...args) {
+                            oldLog(...args)
+                            consoleDiv.innerHTML += args.join(' ') + "<br>"
+                            }
+                          
+                            window.onerror = function(message, source, lineno, colno, error) {
+                              consoleDiv.innerHTML += "<span style='color:red;'> ERROR: " + message + "</span><br>"
+                            }
+                            
+                            try {
+                                ${msg.js}
+                            } catch (e) {
+                              consoleDiv.innerHTML += "<span style='color:red'>" + e + "</span><br>"
+                            }
+                              
+                          </script>
+                    </body>
+                    </html>
+    `
+  }
+})
